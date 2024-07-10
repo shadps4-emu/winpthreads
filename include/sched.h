fca7312d016fa99c1d40a0b1234f317753693f03
DanielSvoboda <daniel.svoboda@hotmail.com>
2024-07-10 00:21:59 -0300

    Support SCHED_ULE and SCHED_4BSD

--- include/sched.h
+++ include/sched.h
@@ -38,8 +38,10 @@
 #define SCHED_OTHER     0
 #define SCHED_FIFO      1
 #define SCHED_RR        2
+#define SCHED_ULE       3
+#define SCHED_4BSD      4
 #define SCHED_MIN       SCHED_OTHER
-#define SCHED_MAX       SCHED_RR
+#define SCHED_MAX       SCHED_4BSD
 
 struct sched_param {
   int sched_priority;
